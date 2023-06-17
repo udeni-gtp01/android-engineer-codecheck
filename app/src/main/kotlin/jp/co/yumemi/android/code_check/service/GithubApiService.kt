004aee66ae03df9aba2e46870eb9ee0725afca95
udenisilva <ukui.silva@gmail.com>
2023-06-17 05:43:56 +0530

    add ApiService

--- app/src/main/kotlin/jp/co/yumemi/android/code_check/service/GithubApiService.kt
+++ app/src/main/kotlin/jp/co/yumemi/android/code_check/service/GithubApiService.kt
@@ -1,5 +1,13 @@
 package jp.co.yumemi.android.code_check.service
 
-class GithubApiService {
+import dagger.Provides
+import jp.co.yumemi.android.code_check.constants.Constant
+import jp.co.yumemi.android.code_check.model.ServerResponse
+import retrofit2.Response
+import retrofit2.http.GET
+import javax.inject.Singleton
 
+interface GithubApiService {
+    @GET(Constant.ENDPOINT_REPOSITORY)
+    suspend fun getRepositories():Response<ServerResponse>
 }
