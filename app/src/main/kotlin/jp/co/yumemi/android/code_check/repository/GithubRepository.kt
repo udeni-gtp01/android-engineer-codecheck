df5b1ae73d9c0d647c739e7c2ec3541c924ab8b8
udenisilva <ukui.silva@gmail.com>
2023-06-17 05:49:29 +0530

    add GithubRepository

--- app/src/main/kotlin/jp/co/yumemi/android/code_check/repository/GithubRepository.kt
+++ app/src/main/kotlin/jp/co/yumemi/android/code_check/repository/GithubRepository.kt
@@ -1,7 +1,16 @@
 package jp.co.yumemi.android.code_check.repository
 
+import jp.co.yumemi.android.code_check.model.ServerResponse
 import jp.co.yumemi.android.code_check.service.GithubApiService
+import javax.inject.Inject
 
-class GithubRepository(githubApiService: GithubApiService) {
-
+class GithubRepository @Inject constructor(private val githubApiService: GithubApiService) {
+    private suspend fun getRepositoriesFromRemoteService():ServerResponse?{
+        var repositories:ServerResponse?=null
+        val response=githubApiService.getRepositories()
+        if (response.isSuccessful){
+            repositories=response.body()
+        }
+        return repositories
+    }
 }
