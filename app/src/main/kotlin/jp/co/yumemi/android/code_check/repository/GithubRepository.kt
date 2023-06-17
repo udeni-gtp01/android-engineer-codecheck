7dcc413c87a8d770e5da416287e7e10d2d8d250c
udenisilva <ukui.silva@gmail.com>
2023-06-17 05:52:03 +0530

    update GithubRepository

--- app/src/main/kotlin/jp/co/yumemi/android/code_check/repository/GithubRepository.kt
+++ app/src/main/kotlin/jp/co/yumemi/android/code_check/repository/GithubRepository.kt
@@ -2,9 +2,17 @@ package jp.co.yumemi.android.code_check.repository
 
 import jp.co.yumemi.android.code_check.model.ServerResponse
 import jp.co.yumemi.android.code_check.service.GithubApiService
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
 class GithubRepository @Inject constructor(private val githubApiService: GithubApiService) {
+
+    suspend fun getRepositoriesFromDataSource():ServerResponse{
+        withContext(Dispatchers.IO){
+            return@withContext getRepositoriesFromRemoteService()
+        }
+    }
     private suspend fun getRepositoriesFromRemoteService():ServerResponse?{
         var repositories:ServerResponse?=null
         val response=githubApiService.getRepositories()
